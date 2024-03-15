9f2247b2cdf035e6a31da022deda94eca495eb20
kenogihara <kenogihara808@gmail.com>
2024-03-14 23:01:58 -0700

    bruh

--- src/MyHashTableTester.java
+++ src/MyHashTableTester.java
@@ -1,12 +1,13 @@
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyHashTableTester {
     MyHashTable employees;
     MyHashTable countries;
+    MyHashTable test;
     String[] names = {"aria", "ken", "ricky", "andrew", "arthur", "annie", "dom", "noah", "nacho"};
     String[] areas = {"america", "new zealand", "china", "switzerland", "ireland", "greenland",
             "japan", "zimbabwe", "nigeria", "korea"};
@@ -14,6 +15,7 @@ class MyHashTableTester {
     void setUp() {
         employees = new MyHashTable(10);
         countries = new MyHashTable(10);
+        test = new MyHashTable();
     }
     @Test
     void constructorOne() {
@@ -61,34 +63,74 @@ class MyHashTableTester {
         assertTrue(countries.delete("korea")); // so there should be 9 elements after this
         assertFalse(countries.delete("russia"));
 
-//        assertEquals(9, countries.capacity() - countries.size());
-//        countries.insert("france");
-//        countries.insert("germany");
-//        assertEquals(11, countries.size());
-//        assertEquals(20, countries.capacity());
+        assertEquals(1, countries.capacity() - countries.size());
+        countries.insert("france");
+        countries.insert("germany");
+        countries.insert("kazakstan");
+        assertEquals(12, countries.size());
+        assertEquals(20, countries.capacity());
+        assertFalse(countries.delete("francee"));
     }
 
     @org.junit.jupiter.api.Test
     void lookup() {
+        for (String country : areas) {
+            countries.insert(country);
+        }
+        assertThrows(NullPointerException.class, () -> countries.lookup(null));
+        assertTrue(countries.lookup("japan"));
+        assertTrue(countries.lookup("china"));
+        assertFalse(countries.lookup("United States of America"));
     }
 
     @org.junit.jupiter.api.Test
     void returnAll() {
+        for (String name : names) {
+            employees.insert(name);
+        }
+        System.out.println(Arrays.toString(employees.returnAll()));
     }
 
     @org.junit.jupiter.api.Test
     void size() {
+        for (String country : areas) {
+            countries.insert(country);
+        }
+        countries.insert("portugal");
+        assertEquals(11, countries.size());
+        countries.insert("argentina");
+        assertEquals(12, countries.size());
+        countries.insert("chile");
+        assertEquals(13, countries.size());
     }
 
     @org.junit.jupiter.api.Test
     void capacity() {
+        assertEquals(20, test.capacity());
+        for (String country : areas) {
+            test.insert(country);
+        }
+        for (String name : names) {
+            test.insert(name);
+        }
+        //there should be 19 elements currently in test object
+        String[] characters = {"mario", "luigi", "wario"};
+        for (String character : characters) {
+            test.insert(character);
+        }
+        assertEquals(40, test.capacity());
+        assertEquals(22, test.size());
+        assertEquals(10, countries.capacity());
+        assertEquals(10, employees.capacity());
     }
 
     @org.junit.jupiter.api.Test
     void getStatsLog() {
+        System.out.println(employees.getStatsLog());
     }
 
     @org.junit.jupiter.api.Test
     void printTable() {
+//        String.valueOf(employees.printTable());
     }
 }
